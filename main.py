3b8f7d496381bd755cd82c95a47beb87d95223c6
Brogan <brogancle@gmail.com>
2018-07-27 20:12:25 -0500

    Add new patterns, move some code to patterns.py, and start time controls

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import datetime
 import calendar
 from patterns import *
 
@@ -15,63 +14,32 @@ LED_STRIP = ws.SK6812_STRIP_RGBW
 # Special days
 # Use datetime.date(year, month, day)
 christmas = datetime.date(2018, 12, 25)
-
-# Time of school periods (2:35 dismissal)
-first_start = datetime.time(7, 25)
-first_end = datetime.time(8, 12)
-second_start = datetime.time(8, 17)
-second_end = datetime.time(9, 4)
-third_start = datetime.time(9, 9)
-third_end = datetime.time(10, 2)
-fourth_start = datetime.time(10, 7)
-fourth_end = datetime.time(10, 59)
-fifth_start = datetime.time(11, 3)
-fifth_end = datetime.time(11, 55)
-sixth_start = datetime.time(11, 59)
-sixth_end = datetime.time(12, 51)
-seventh_start = datetime.time(12, 56)
-seventh_end = datetime.time(1, 43)
-eighth_start = datetime.time(1, 48)
-eighth_end = datetime.time(2, 35)
+zee = datetime.date(2019, 2, 3)
 
 # Initialize the pixels
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 strip.begin()
 
 
-def school_day(current_time, class_time, passing):
-    """Change the pattern throughout the school day so different patterns happen during class,
-     and others during passing period"""
-    if (first_start <= current_time <= first_end
-            or second_start <= current_time <= second_end
-            or third_start <= current_time <= third_end
-            or fourth_start <= current_time <= fourth_end
-            or fifth_start <= current_time <= fifth_end
-            or sixth_start <= current_time <= sixth_end
-            or seventh_start <= current_time <= seventh_end
-            or eighth_start <= current_time <= eighth_end):
-        class_time()
-    else:
-        passing()
-
-
 while True:
-    date = datetime.datetime.today()
+    current_date = datetime.datetime.today()
     # The current day, weekday, and time at each iteration
-    day = date.date()
-    weekday = calendar.day_name(date)
-    time = date.time()
+    day = current_date.date()
+    time = current_date.time()
 
     if time >= datetime.time(17) or time <= datetime.time(5):
         """Turn the pixels off between 5PM and 5AM"""
-        clear(strip)
-    elif date == christmas:
+        turn_off(strip)
+    elif day == christmas:
         """Run special pattern for Christmas"""
+        # todo - make special christmas pattern
         school_day(time,
                    color_wipe(strip, Color(255, 255, 0)),
                    color_wipe(strip, Color(255, 255, 0)))
+    elif day == zee:
+        flash(strip, Color(255, 20, 147), 1000)
     else:
-        """Run everyday """
+        """Run everyday an event isn't taking place"""
         school_day(time,
                    color_wipe(strip, Color(255, 255, 0)),
                    color_wipe(strip, Color(255, 255, 0)))
--- patterns.py
+++ patterns.py
@@ -1,9 +1,45 @@
 import time
-
+import datetime
 from neopixel import *
 
+off = Color(0, 0, 0)
 
-def color_wipe(strip, color, wait_ms=50):
+# Time of school periods (2:35 dismissal)
+first_start = datetime.time(7, 25)
+first_end = datetime.time(8, 12)
+second_start = datetime.time(8, 17)
+second_end = datetime.time(9, 4)
+third_start = datetime.time(9, 9)
+third_end = datetime.time(10, 2)
+fourth_start = datetime.time(10, 7)
+fourth_end = datetime.time(10, 59)
+fifth_start = datetime.time(11, 3)
+fifth_end = datetime.time(11, 55)
+sixth_start = datetime.time(11, 59)
+sixth_end = datetime.time(12, 51)
+seventh_start = datetime.time(12, 56)
+seventh_end = datetime.time(1, 43)
+eighth_start = datetime.time(1, 48)
+eighth_end = datetime.time(2, 35)
+
+
+def school_day(current_time, class_time, passing):
+    """Change the pattern throughout the school day so different patterns happen during class,
+     and others during passing period"""
+    if (first_start <= current_time <= first_end
+            or second_start <= current_time <= second_end
+            or third_start <= current_time <= third_end
+            or fourth_start <= current_time <= fourth_end
+            or fifth_start <= current_time <= fifth_end
+            or sixth_start <= current_time <= sixth_end
+            or seventh_start <= current_time <= seventh_end
+            or eighth_start <= current_time <= eighth_end):
+        class_time()
+    else:
+        passing()
+
+
+def color_wipe(strip, color, wait_ms=250):
     """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
@@ -11,7 +47,55 @@ def color_wipe(strip, color, wait_ms=50):
         time.sleep(wait_ms / 1000.0)
 
 
-def clear(strip):
+def turn_off(strip):
+    """Turn off all the pixels"""
     for i in range(strip.numPixels):
-        strip.setPixelColor(Color(0,0,0,0))
+        strip.setPixelColor(i, off)
     strip.show()
+
+
+def flash(strip, color, wait_ms=500):
+    """Flash pixels on and off at a set rate"""
+    for i in range(strip.numPixels):  # On
+        strip.setPixelColor(i, color)
+    strip.show()
+    time.sleep(wait_ms/1000)
+
+    turn_off(strip)
+    time.sleep(wait_ms/1000)
+
+
+def wheel(wheel_pos):
+    wheel_pos = 255 - wheel_pos
+    if wheel_pos < 85:
+        return Color(255 - wheel_pos * 3, 0, wheel_pos * 3)
+    elif wheel_pos < 170:
+        wheel_pos -= 85
+        return Color(0, wheel_pos * 3, 255 - wheel_pos * 3)
+    else:
+        wheel_pos -= 170
+        return Color(wheel_pos * 3, 255 - wheel_pos * 3, 0)
+
+
+def rainbow_cycle(strip, wait_ms=50):
+    for j in range(256 * 5):
+        for i in range(strip.numPixels):
+            strip.setPixelColor(i, wheel(((i * 256 / strip.numPixels) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000)
+
+
+def flag(strip, wait_ms=100):
+    for i in range(strip.numPixels):
+        for blue in range(strip.numPixels/4):
+            if blue % 3 == 0:
+                strip.setPixelColor((blue + i) % strip.numPixels, Color(0, 0, 0, 255))
+            else:
+                strip.setPixelColor((blue + i) % strip.numPixels, Color(0, 0, 255, 0))
+        for red in range(strip.numPixels - strip.numPixels/4):
+            if red % 2 == 0:
+                strip.setPixelColor((red + i + 10) % 40, Color(255, 0, 0, 0))
+            else:
+                strip.setPixelColor((red + i + 10) % 40, Color(0, 0, 0, 255))
+        strip.show()
+        time.sleep(wait_ms/1000)
